ef6869bc0a5009022e2ee45f2dcd1bccc24782fc
Hiroki Yokoyama <hiroki.yokoyama@okadanet.org>
2018-06-30 21:55:47 +0900

    added input image to the summary

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 For details, see https://ai.googleblog.com/2018/06/self-supervised-tracking-via-video.html.
 
 ## Requirements
-* *Latest version* of youtube-dl
+* *Latest version* of youtube-dl (see https://rg3.github.io/youtube-dl/download.html)
 * ffmpeg
 
 ## Usage
--- train.py
+++ train.py
@@ -44,15 +44,17 @@ if not os.path.exists(model_dir):
 #LAYER = 'resnet_v1_101/block2/unit_3/bottleneck_v1/conv3'
 LAYER = 'resnet_v1_101/block2/unit_3/bottleneck_v1'
 DIM = 256
-    
+
 data = create_ref_target_generator(NUM_REF, NUM_TARGET).repeat().map(lambda x,y: tf.concat([x,y], 0))
-image_gen = data.map(lambda x: tf.image.resize_images(x[:,:,:,0:1], [256,256])).make_one_shot_iterator()
+image_gen = data.map(lambda x: tf.image.resize_images(x, [256,256])).make_one_shot_iterator()
 label_gen = data.map(lambda x: tf.image.resize_images(x, [32,32])).map(lab_to_labels).make_one_shot_iterator()
 
-images = image_gen.get_next()
-labels = label_gen.get_next()
+images = image_gen.get_next(name='images') # Lab image, [N,256,256,3], can be fed at sess.run
+labels = label_gen.get_next(name='labels') # color labels (or other categorical data), [N,32,32,d], can be fed at sess.run
+
+# extract features from gray scale image (only L channel) using CNN
 with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-    _, end_points = resnet_v1.resnet_v1_101(images, 1000, is_training=True)
+    _, end_points = resnet_v1.resnet_v1_101(images[:,:,:,0:1], 1000, is_training=True)
 with slim.arg_scope([slim.conv2d], stride=1, padding='SAME',
                     activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
   with slim.arg_scope([slim.batch_norm], is_training=True):
@@ -60,22 +62,28 @@ with slim.arg_scope([slim.conv2d], stride=1, padding='SAME',
       feature_map = slim.conv2d(net, DIM, [1,1],
                                 activation_fn=None,
                                 normalizer_fn=None)
+feature_map = tf.identity(feature_map, name='features') # using tf.identity to rename so that it can be easily fetched/fed at sess.run
 
+# predict the color (or other category) on the basis of the features
 end_points = estimator(feature_map[:NUM_REF], tf.one_hot(labels[:NUM_REF], num_clusters),
                        feature_map[NUM_REF:], labels[NUM_REF:])
-prediction = end_points['predictions']
+prediction = tf.identity(end_points['predictions'], name='predictions')
 prediction_lab = labels_to_lab(prediction)
 loss = tf.reduce_sum(end_points['losses'])
+train_op = tf.train.AdamOptimizer().minimize(loss)
+
+# summaries
 loss_summary = tf.summary.scalar('loss', loss)
+ph_target_img = tf.placeholder(tf.float32, shape=[1,None,None,3])
 ph_vis_pred = tf.placeholder(tf.float32, shape=[1,None,None,3])
 ph_vis_feat = tf.placeholder(tf.float32, shape=[1,None,None,3])
-image_summary = tf.summary.merge([tf.summary.image('visualized_prediction', ph_vis_pred),
+image_summary = tf.summary.merge([tf.summary.image('target_image', ph_target_img),
+                                  tf.summary.image('visualized_prediction', ph_vis_pred),
                                   tf.summary.image('visualized_feature', ph_vis_feat)])
-train_op = tf.train.AdamOptimizer().minimize(loss)
 
 sess = tf.Session()
 saver = tf.train.Saver()
-writer = tf.summary.FileWriter(model_dir)
+writer = tf.summary.FileWriter(os.path.join(model_dir, 'model.ckpt'))
 sess.run(tf.global_variables_initializer())
 
 import cv2
@@ -83,13 +91,21 @@ from sklearn.decomposition import PCA
 pca = PCA(n_components=3)
 
 for i in xrange(100000):
-    feat, pred, _, summary = sess.run([feature_map,
-                                       prediction_lab,
-                                       train_op,
-                                       loss_summary])
-    writer.add_summary(summary, i)
-    
-    if i % 100 == 0:
+    if i % 100 != 0:
+        _, summary = sess.run([train_op, loss_summary])
+        # summarize only loss
+        writer.add_summary(summary, i)
+    else:
+        img, feat, pred, _, summary = sess.run([images,
+                                                feature_map,
+                                                prediction_lab,
+                                                train_op,
+                                                loss_summary])
+        # summarize loss
+        writer.add_summary(summary, i)
+
+        # and images (doing some stuff to visualize outside the tf session)
+        target_img = cv2.cvtColor(img[NUM_REF], cv2.COLOR_LAB2RGB)
         vis_pred = cv2.cvtColor(pred[0], cv2.COLOR_LAB2RGB)
         feat_flat = feat[NUM_REF].reshape(-1, feat.shape[-1])
         pca.fit(feat_flat)
@@ -97,9 +113,11 @@ for i in xrange(100000):
         feat_flat /= np.abs(feat_flat).max()
         feat_flat = (feat_flat + 1) / 2
         vis_feat = feat_flat.reshape(feat.shape[1:3]+(3,))
-        summary = sess.run(image_summary, {ph_vis_pred: [vis_pred],
+        summary = sess.run(image_summary, {ph_target_img: [target_img],
+                                           ph_vis_pred: [vis_pred],
                                            ph_vis_feat: [vis_feat]})
         writer.add_summary(summary, i)
         
     if i % 1000 == 0:
+        # save the model
         saver.save(sess, model_dir, global_step=i)
