2388624acdfcc90df30604a1beeb0a006bd7afe6
Hiroki Yokoyama <hiroki.yokoyama@okadanet.org>
2018-07-01 12:28:09 +0900

    separated CNN definitions

---
+++ nets.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+slim = tf.contrib.slim
+
+def feature_extractor_resnet(images,
+                             layer = 'resnet_v1_101/block2/unit_3/bottleneck_v1',
+                             dim = 256):
+    from tensorflow.contrib.slim.python.slim.nets import resnet_v1
+    
+    with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+        _, end_points = resnet_v1.resnet_v1_101(images, 1000, is_training=True)
+    with slim.arg_scope([slim.conv2d], stride=1, padding='SAME',
+                        activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
+        with slim.arg_scope([slim.batch_norm], is_training=True):
+            net = end_points[layer]
+            # you can add convolutional layers here
+
+            # the last layer without activation function
+            feature_map = slim.conv2d(net, dim, [1,1],
+                                      activation_fn=None,
+                                      normalizer_fn=None)
+    return feature_map
+
+def colorizer(ref_features, ref_labels, target_features, target_labels=None):
+    # ref_features: [N_REF,H,W,D], feature map from reference frames
+    # ref_labels: [N_REF,H,W,d], category probabilities or one-hot vectors from reference frames
+    # target_features: [N_TARGET,H,W,D], feature map from target frames
+    # target_labels: ([N_TARGET,H,W,d],float) or ([N_TARGET,H,W],int), ground truth category probabilities or indices
+    # (D denotes feature dimension, d denotes number of categories)
+    bhw = tf.shape(target_features)[:-1]
+    dim = tf.shape(ref_features)[-1]
+    ref_features = tf.reshape(ref_features, [-1,1,dim])
+    target_features = tf.reshape(target_features, [1,-1,dim])
+    inner = tf.reduce_sum(ref_features * target_features, -1)
+    weight_mat = tf.nn.softmax(inner, 1)
+
+    ref_labels = tf.convert_to_tensor(ref_labels)
+    if ref_labels.dtype in [tf.float16, tf.float32, tf.float64]:
+        ref_labels = tf.reshape(ref_labels, [-1,1,tf.shape(ref_labels)[-1]])
+    else:
+        raise ValueError('ref_labels must be one-hot or probabilities, not indices')
+        #ref_labels = tf.one_hot(tf.reshape(ref_labels,[-1,1]), )
+    prediction = tf.reduce_sum(tf.expand_dims(weight_mat, -1) * ref_labels, 0)
+    prediction = tf.reshape(prediction, tf.concat([bhw, [-1]], 0))
+    end_points = {'logit_matrix': inner, 'weight_matrix': weight_mat, 'predictions': prediction}
+    
+    if target_labels is not None:
+        target_labels = tf.convert_to_tensor(target_labels)
+        if target_labels.dtype in [tf.float16, tf.float32, tf.float64]:
+            fn = tf.nn.softmax_cross_entropy_with_logits
+        else:
+            fn = tf.nn.sparse_softmax_cross_entropy_with_logits
+        end_points['losses'] = fn(logits=prediction, labels=target_labels)
+    return end_points
--- train.py
+++ train.py
@@ -1,78 +1,43 @@
 import tensorflow as tf
 import numpy as np
 import os
-slim = tf.contrib.slim
-
-def estimator(ref_features, ref_labels, target_features, target_labels=None):
-    bhw = tf.shape(target_features)[:-1]
-    dim = tf.shape(ref_features)[-1]
-    ref_features = tf.reshape(ref_features, [-1,1,dim])
-    target_features = tf.reshape(target_features, [1,-1,dim])
-    inner = tf.reduce_sum(ref_features * target_features, -1)
-    weight_mat = tf.nn.softmax(inner, 1)
-
-    ref_labels = tf.convert_to_tensor(ref_labels)
-    if ref_labels.dtype in [tf.float16, tf.float32, tf.float64]:
-        ref_labels = tf.reshape(ref_labels, [-1,1,tf.shape(ref_labels)[-1]])
-    else:
-        raise ValueError('ref_labels must be one-hot')
-        #ref_labels = tf.one_hot(tf.reshape(ref_labels,[-1,1]), )
-    prediction = tf.reduce_sum(tf.expand_dims(weight_mat, -1) * ref_labels, 0)
-    prediction = tf.reshape(prediction, tf.concat([bhw, [-1]], 0))
-    end_points = {'logit_matrix': inner, 'weight_matrix': weight_mat, 'predictions': prediction}
-    
-    if target_labels is not None:
-        target_labels = tf.convert_to_tensor(target_labels)
-        if target_labels.dtype in [tf.float16, tf.float32, tf.float64]:
-            fn = tf.nn.softmax_cross_entropy_with_logits
-        else:
-            fn = tf.nn.sparse_softmax_cross_entropy_with_logits
-        end_points['losses'] = fn(logits=prediction, labels=target_labels)
-    return end_points
-
-from tensorflow.contrib.slim.python.slim.nets import resnet_v1
+from nets import feature_extractor_resnet, colorizer
 from dataset import create_ref_target_generator
 from clustering import lab_to_labels, labels_to_lab
 from clustering import num_clusters
 
 NUM_REF = 3
 NUM_TARGET = 1
-model_dir = os.path.join(os.path.dirname(__file__), 'data', 'model')
-if not os.path.exists(model_dir):
-    os.mkdir(model_dir)
-#YOLOv3 upsample upper layer and concat with lower layer
-#LAYER = 'resnet_v1_101/block2/unit_3/bottleneck_v1/conv3'
-LAYER = 'resnet_v1_101/block2/unit_3/bottleneck_v1'
-DIM = 256
+FEATURE_DIM = 256
+LEARNING_RATE = 0.01
+MODEL_DIR = os.path.join(os.path.dirname(__file__), 'data', 'model')
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
 
+##### create dataset
 data = create_ref_target_generator(NUM_REF, NUM_TARGET).repeat().map(lambda x,y: tf.concat([x,y], 0))
-image_gen = data.map(lambda x: tf.image.resize_images(x, [256,256])).make_one_shot_iterator()
-label_gen = data.map(lambda x: tf.image.resize_images(x, [32,32])).map(lab_to_labels).make_one_shot_iterator()
+image_gen = data.map(lambda x: tf.image.resize_images(x, [256,256]))
+label_gen = data.map(lambda x: tf.image.resize_images(x, [32,32])).map(lab_to_labels)
 
-images = image_gen.get_next(name='images') # Lab image, [N,256,256,3], can be fed at sess.run
-labels = label_gen.get_next(name='labels') # color labels (or other categorical data), [N,32,32,d], can be fed at sess.run
+# Lab image, [N,256,256,3], can be fed at sess.run
+images = image_gen.make_one_shot_iterator().get_next(name='images')
+# color labels (or other categorical data), [N,32,32,d], can be fed at sess.run
+labels = label_gen.make_one_shot_iterator().get_next(name='labels')
 
-# extract features from gray scale image (only L channel) using CNN
-with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-    _, end_points = resnet_v1.resnet_v1_101(images[:,:,:,0:1], 1000, is_training=True)
-with slim.arg_scope([slim.conv2d], stride=1, padding='SAME',
-                    activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
-  with slim.arg_scope([slim.batch_norm], is_training=True):
-      net = end_points[LAYER]
-      feature_map = slim.conv2d(net, DIM, [1,1],
-                                activation_fn=None,
-                                normalizer_fn=None)
-feature_map = tf.identity(feature_map, name='features') # using tf.identity to rename so that it can be easily fetched/fed at sess.run
+##### extract features from gray scale image (only L channel) using CNN
+feature_map = feature_extractor_resnet(images[:,:,:,0:1], dim=FEATURE_DIM)
+# rename with tf.identity so that it can be easily fetched/fed at sess.run
+feature_map = tf.identity(feature_map, name='features')
 
-# predict the color (or other category) on the basis of the features
-end_points = estimator(feature_map[:NUM_REF], tf.one_hot(labels[:NUM_REF], num_clusters),
+##### predict the color (or other category) on the basis of the features
+end_points = colorizer(feature_map[:NUM_REF], tf.one_hot(labels[:NUM_REF], num_clusters),
                        feature_map[NUM_REF:], labels[NUM_REF:])
 prediction = tf.identity(end_points['predictions'], name='predictions')
 prediction_lab = labels_to_lab(prediction)
 loss = tf.reduce_mean(end_points['losses'])
-train_op = tf.train.AdamOptimizer().minimize(loss)
+train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
 
-# summaries
+##### summaries
 loss_summary = tf.summary.scalar('loss', loss)
 ph_target_img = tf.placeholder(tf.float32, shape=[1,None,None,3])
 ph_vis_pred = tf.placeholder(tf.float32, shape=[1,None,None,3])
@@ -80,12 +45,14 @@ ph_vis_feat = tf.placeholder(tf.float32, shape=[1,None,None,3])
 image_summary = tf.summary.merge([tf.summary.image('target_image', ph_target_img),
                                   tf.summary.image('visualized_prediction', ph_vis_pred),
                                   tf.summary.image('visualized_feature', ph_vis_feat)])
+writer = tf.summary.FileWriter(MODEL_DIR)
 
-sess = tf.Session()
+##### create session and initialize
 saver = tf.train.Saver()
-writer = tf.summary.FileWriter(model_dir)
+sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+##### main loop
 import cv2
 from sklearn.decomposition import PCA
 pca = PCA(n_components=3)
@@ -120,4 +87,4 @@ for i in xrange(100000):
         
     if i % 1000 == 0:
         # save the model
-        saver.save(sess, os.path.join(model_dir, 'model.ckpt'), global_step=i)
+        saver.save(sess, os.path.join(MODEL_DIR, 'model.ckpt'), global_step=i)
