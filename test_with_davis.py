cd76e71b6a323a464ba5d9413bb6cf2f245b6a68
Hiroki Yokoyama <javawriter.hy@gmail.com>
2020-07-26 20:10:36 +0900

    update test_with_davis.py

--- test_with_davis.py
+++ test_with_davis.py
@@ -6,13 +6,17 @@ import numpy as np
 import os
 from nets import create_feature_extractor, Colorizer
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 MODEL_DIR = os.path.join(DATA_DIR, 'model')
 
-def apply_mask(image, mask, colors):
-  return mask[:,:,0:1] * image \
-      + (mask[:,:,1:,np.newaxis] * colors[1:].reshape(1,1,-1,3)).sum(2)
+def apply_mask(images, masks, colors):
+  colors = tf.reshape(colors[1:], [1,1,1,-1,3])
+  bg = masks[...,0:1] * images
+  fg = masks[...,1:,tf.newaxis] * colors
+  fg = tf.reduce_sum(fg, axis=3)
+  return bg + fg
 
 class Davis:
   def __init__(self, base_dir):
@@ -61,6 +65,48 @@ class Davis:
         count += 1
     return mask, np.array(colors)
 
+def track_regions(colorizer, images, labels,
+                  temperature = 0.5):
+  images_gray = tf.reduce_mean(images, axis=3, keepdims=True) * 2. - 1.
+  images_gray = tf.image.resize(images_gray, [256,256])
+  n_images, orig_h, orig_w, c = tf.unstack(tf.shape(images))
+
+  out_shape = colorizer.feature_extractor.compute_output_shape(
+      images_gray.shape)
+  labels_downsampled = tf.image.resize(labels, out_shape[1:3])
+
+  labels_array = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
+  labels_array = labels_array.write(0, labels_downsampled)
+
+  def loop_body(i, images, labels):
+    begin = tf.maximum(i-3, 0)
+    ref_images = images[begin:i]
+    target_images = images[i:i+1]
+    ref_labels = labels.gather(tf.range(begin, i))
+    target_labels = colorizer({
+        'reference_images': ref_images[tf.newaxis],
+        'target_images': target_images[tf.newaxis],
+        'reference_labels': ref_labels[tf.newaxis],
+        'temperature': temperature})
+    labels = labels.write(i, target_labels[0,0])
+    return i+1, images, labels
+  loop_cond = lambda i, _, labels: i < n_images
+  loop_vars = tf.constant(1, dtype=tf.int32), images_gray, labels_array
+  i, _, labels_array = tf.while_loop(loop_cond, loop_body, loop_vars)
+  
+  return tf.image.resize(labels_array.stack(), [orig_h, orig_w])
+
+def make_animation(frames):
+  plt.figure(figsize=(frames[0].shape[1]/72.0, frames[0].shape[0]/72.0), dpi=72)
+  patch = plt.imshow(frames[0])
+  plt.axis('off')
+
+  def animate(i):
+    patch.set_data(frames[i])
+
+  anim = FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=1000/30.0)
+  return anim
+    
 def main(data_dir=DATA_DIR, model_dir=MODEL_DIR):
   davis = Davis(base_dir=data_dir)
   
@@ -71,36 +117,18 @@ def main(data_dir=DATA_DIR, model_dir=MODEL_DIR):
 
   image_set = np.random.choice(davis.image_sets)
 
-  annotation_large, colors = davis.get_annotation(image_set)
-  colors = colors / 255.
+  annotations, colors = davis.get_annotation(image_set)
+  colors = tf.constant(colors / 255., dtype=tf.float32)
   n_colors = colors.shape[0]
-  annotation_large = tf.one_hot(annotation_large, n_colors).numpy()
-  annotation = cv2.resize(annotation_large, (32,32))
+  initial_labels = tf.one_hot(annotations, n_colors)
 
-  all_images = davis.get_images(image_set) / 255.
-  all_images_gray = all_images.mean(3) * 2. - 1.
-  all_images_gray = np.array([cv2.resize(img, (256,256)) for img in all_images_gray])
-  all_images_gray = all_images_gray[...,np.newaxis]#.repeat(3, -1)
+  images = tf.constant(davis.get_images(image_set) / 255.,
+                       dtype = tf.float32)
 
-  plt.subplot(3,1,1)
-  plt.imshow(apply_mask(all_images[0], annotation_large, colors))
-  plt.axis('off')
-
-  annotations = annotation[np.newaxis].repeat(3, 0)
-  for t in range(1, 3):
-    images = all_images_gray[[max(t-3,0),max(t-2,0),t-1,t]]
-    #print(images[np.newaxis,:3].shape)
-    predictions = colorizer({
-        'reference_images': np.float32(images[np.newaxis,:3]),
-        'target_images': np.float32(images[np.newaxis,3:]),
-        'reference_labels': np.float32(annotations[np.newaxis]),
-        'temperature': 0.5})
-    annotations = np.roll(annotations, -1, 0)
-    annotations[-1] = predictions[0,0].numpy()
-    mask = cv2.resize(predictions[0,0].numpy(), (all_images.shape[2], all_images.shape[1]))
-    plt.subplot(3,1,t+1)
-    plt.imshow(apply_mask(all_images[t], mask, colors))
-    plt.axis('off')
+  labels = track_regions(colorizer, images, initial_labels,
+                         temperature = 0.5)
+  results = apply_mask(images, labels, colors)
+  make_animation(results)
   plt.show()
   
 if __name__=='__main__':
